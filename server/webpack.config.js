00ec2236009ac2e8c969819386502eccf536157e
Kunal Takalkar <kunaltakalkar.123@gmail.com>
2023-03-20 18:13:53 +0530

    build succesful code2

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry:  path.join(__dirname, 'src', index.js),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
