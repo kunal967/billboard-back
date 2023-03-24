ba95e2468a364765041607cb5b1d2ccda5ed533b
Kunal Takalkar <kunaltakalkar.123@gmail.com>
2023-03-24 11:24:56 +0530

    fixed bugs

--- server/src/db.js
+++ server/src/db.js
@@ -1,15 +1,15 @@
-
 const mySql = require("mysql");
 let instance = null;
-require("dotenv").config();
+require('dotenv').config()
 
-const pool = mySql.createPool({
-  connectionLimit: 10,
-  host: process.env.HOST,
-  user: process.env.USER_NAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+const connection = mySql.createPool({
+connectionLimit : 10,
+host : process.env.HOST,
+user : process.env.USER_NAME,
+password: process.env.PASSWORD,
+database: process.env.DATABASE
 });
+
 
 class DbService {
   static getDbServiceInstance() {
@@ -18,47 +18,40 @@ class DbService {
   async getData() {
     try {
       const response = await new Promise((resolve, reject) => {
-        const query = "SELECT * FROM billboardinfo WHERE Status = 1";
-        pool.query(query, (err, result) => {
-          if (err) {
-            console.error(err);
-            return reject(err);
-          }
+        const query = `SELECT * FROM billboardinfo WHERE Status = 1`;
+        
+        connection.query(query, (err, result) => {
           console.log("database connected succesfully");
-          return resolve(result);
+          if (err) reject(new Error(err.message));
+          resolve(result);
         });
       });
       return response;
     } catch (err) {
-      console.error(err);
-      return null;
-    } finally {
-      pool.end();
+      console.log(err);
     }
   }
-  
-  async postData(Name, date, route, marker_type, distance) {
+  async postData(Name,date,route,marker_type,distance) {
     try {
       const InsertId = await new Promise((resolve, reject) => {
         const query =
           "INSERT INTO markerdistancedata (Name,date,route,marker_type,distance) VALUES (?,?,?,?,?);";
-        pool.query(
+        connection.query(
           query,
           [Name, date, route, marker_type, distance],
           (err, result) => {
             if (err) reject(new Error(err.message));
             resolve(result.InsertId);
-          }
-        );
-      });
-      return {
+        });
+    });
+    return{
         id: InsertId,
         Name: Name,
-        date: date,
+        date : date,
         route: route,
         marker_type: marker_type,
         distance: distance,
-      };
+    }
     } catch (error) {
       console.log(error.message);
     }
